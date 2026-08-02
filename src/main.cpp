3546840cfb880ca21e3f9983aaf322d0b611b029
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-02 05:13:32 -0400

    board design

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,44 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow win(VideoMode(900, 969), "Taesef");
-    RectangleShape rec;
-    rec.setSize(Vector2f(169, 167));
-    rec.setOutlineColor(Color::Red);
-    rec.setFillColor(Color::Blue);
-    rec.setPosition({100, 0});
+    RenderWindow win(VideoMode(800, 800), "Taesef");
+    int x,y;
+    Color dark_green(0, 100, 0);
+    Color cream(238, 238, 210);
+    vector<vector<RectangleShape>> v(8, vector<RectangleShape>(8));
+    for(int i=0; i<8; i++)
+    {
+        for(int j=0; j<8; j++)
+        {
+            RectangleShape rec; 
+            rec.setSize(Vector2f(100, 100));
+            if((i+j)%2==0)
+            {
+                rec.setFillColor(cream);
+            }
+            else
+            {
+                rec.setFillColor(dark_green);
+            }
+            rec.setPosition({(float)i*100.0f, (float)j*100.0f});
+            v[i][j] = rec;
+        }
+    }
+
+    Texture kingtex;
+
+    if(!kingtex.loadFromFile("../assets/pieces/black_king.png"))
+    {
+        cout<<"Error\n";
+    }
+
+    Sprite king;
+    king.setTexture(kingtex);
+
+    king.setScale(100.0f/kingtex.getSize().x, 100.0f/kingtex.getSize().y);
+    king.setPosition(400, 300);
+
+
     while(win.isOpen())
     {
         Event e;
@@ -22,9 +54,15 @@ int main()
             }
         }
         win.clear(Color::Black);
-        win.draw(rec);
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                win.draw(v[i][j]);
+            }
+        }
+        win.draw(king);
         win.display();
     }
-
 
 }
