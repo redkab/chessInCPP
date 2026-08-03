5e86600e35831064d8a6b179af43350a32109b33
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-03 10:09:38 -0400

    added move function

---
+++ src/move_func.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//using namespace sf;
+//#include "move_func.h"
+
+void moveFunc(char **board, pair<int, int>start, pair<int, int>end)
+{
+    board[end.first][end.second] = board[start.first][start.second];
+    board[start.first][start.second] = ' ';
+}
+
