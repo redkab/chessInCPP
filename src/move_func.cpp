d5f1c5d939d6af88fc228dc134f52fe5a1ec4ed7
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-03 11:46:28 -0400

    add object vector for moves

--- src/move_func.cpp
+++ src/move_func.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 using namespace std;
+#include<vector>
 //using namespace sf;
-//#include "move_func.h"
+#include "move_func.h"
 
-void moveFunc(char board[8][8], pair<int, int>start, pair<int, int>end)
+void moveFunc(char board[8][8], pair<int, int>start, pair<int, int>end, vector<vector<Sprite>>v)
 {
     if(start != end)
     {
     	board[end.first][end.second] = board[start.first][start.second];
     	board[start.first][start.second] = ' ';
+    	v[end.first][end.second] = v[start.first][start.second];
+    	v[start.first][start.second] = E;
     }
 }
 
