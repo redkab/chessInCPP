77e8036e3860392f6d334240816a1cfef7a99558
tawstar <tawhasan8@gmail.com>
2026-08-04 22:19:26 +0530

    validity checker initation

---
+++ src/move_rules.h
@@ -0,0 +1,10 @@
+#ifndef MOVE_RULES_H
+#define MOVE_RULES_H
+
+#include <utility>
+
+bool isLegal(board[8][8], std::pair<int, int>start, std::pair<int, int>end);
+bool isWhite(char w);
+bool isBlack(char b);
+
+#endif
