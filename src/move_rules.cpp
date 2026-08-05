890bc523827d46941738b06c10e296d277ef58db
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-05 09:39:38 -0400

    added basics

--- src/move_rules.cpp
+++ src/move_rules.cpp
@@ -1,1 +1,16 @@
-tuff
+#include "move_rules.h"
+
+bool isBlack(char p)
+{
+    return p>='a' && p<='z';
+}
+
+bool isWhite(char p)
+{
+    return p>='A' && p<='Z';
+}
+
+bool isTeam(char p, char q)
+{
+    return (p>='A' && p<='Z')&&(q>='A' && q<='Z') || (p>='a' && p<='z') && (q>='a' && q<='z');
+}
