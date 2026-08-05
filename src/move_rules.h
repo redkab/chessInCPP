910126a4d105481308b8e86594b94a58bf23ade6
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-05 09:55:13 -0400

    added func signatures

--- src/move_rules.h
+++ src/move_rules.h
@@ -2,9 +2,12 @@
 #define MOVE_RULES_H
 
 #include <utility>
+#include<cmath>
 
 bool isLegal(board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 bool isWhite(char w);
 bool isBlack(char b);
+bool isTeam(char p, char q);
+bool isValidKnightMove(pair<int, int>start, pair<int, int>end);
 
 #endif
