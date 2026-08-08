9eed7fa764dcf8a1ea72ffa9232f6e169ac60b8a
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-08 01:02:31 -0400

    added all pieces function signatures

--- src/move_rules.h
+++ src/move_rules.h
@@ -8,6 +8,10 @@ bool isLegal(char board[8][8], std::pair<int, int>start, std::pair<int, int>end)
 bool isWhite(char p);
 bool isBlack(char p);
 bool isTeam(char p, char q);
-bool isValidKnightMove(std::pair<int, int>start, std::pair<int, int>end);
+bool isValidKnightMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
+bool isValidRookMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
+bool isValidQueenMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
+bool isValidPawnMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
+bool isValidBishopMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 int sgn(int x);
 #endif
