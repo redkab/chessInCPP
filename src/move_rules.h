e33266982f5471d97891b21981ab9a740b17755b
tawstar <tawhasan8@gmail.com>
2026-08-09 12:21:34 +0530

    header files for king and check func

--- src/move_rules.h
+++ src/move_rules.h
@@ -9,9 +9,11 @@ bool isWhite(char p);
 bool isBlack(char p);
 bool isTeam(char p, char q);
 bool isValidKnightMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
+bool isValidKingMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 bool isValidRookMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 bool isValidQueenMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 bool isValidPawnMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 bool isValidBishopMove(char board[8][8], std::pair<int, int>start, std::pair<int, int>end);
 int sgn(int x);
+bool isInCheck(char board[8][8], bool colour)
 #endif
