b37921fb1e0a30a87542aaf5677e8ece8934e491
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-08-10 09:34:16 -0400

    added pawn promotion

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,25 @@ int main()
     bool flag = true;
     bool turn = true;
     bool over = false;
+    bool promo = false;
+    pair<int, int>promoSquare;
+    bool whitePromo = false;
+    RectangleShape promoMenu;
+    promoMenu.setSize(Vector2f(400, 100));
+    promoMenu.setPosition(200, 350);
+    promoMenu.setFillColor(Color(112, 138, 164));
+    Sprite wpq = wQ, wpr = wR1, wpn = wN1, wpb = wB1;
+    Sprite bpq = bQ, bpr = bR1, bpn = bN1, bpb = bB1;
+
+    wpq.setPosition(200, 350);
+    wpr.setPosition(300, 350);
+    wpn.setPosition(400, 350);
+    wpb.setPosition(500, 350);
+    bpq.setPosition(200, 350);
+    bpr.setPosition(300, 350);
+    bpn.setPosition(400, 350);
+    bpb.setPosition(500, 350);
+
     while(win.isOpen())
     {
         Event e;
@@ -62,6 +81,10 @@ int main()
 
             if(e.type == Event::MouseButtonPressed && e.mouseButton.button == Mouse::Left)
             {
+                if(over)
+                {
+                    continue;
+                }
                 int row = e.mouseButton.y/100;
                 int col = e.mouseButton.x/100;
                 if(row>=8 || row <0 || col >=8 || col <0)
@@ -89,6 +112,51 @@ int main()
                     flag = true;
                     if(isLegal(b, start, end))
                     {
+                        if((b[start.first][start.second] == 'p' && end.first == 7) || (b[start.first][start.second] == 'P' && end.first == 0))
+                        {
+                            promo = true;
+                            promoSquare = end;
+                            whitePromo = isWhite(b[start.first][start.second]);
+                        }
+
+                        if(promo)
+                        {
+                            int x = e.mouseButton.x;
+                            int y = e.mouseButton.y;
+                            char chosenPiece = ' ';
+                            if(y>=350 && y<450)
+                            {
+                                if(x >= 200 && x<300)
+                                {
+                                    chosenPiece = 'Q';
+                                }
+                                else if(x >= 300 && x<400)
+                                {
+                                    chosenPiece = 'R';
+                                }
+                                else if(x >= 400 && x<500)
+                                {
+                                    chosenPiece = 'N';
+                                }
+                                else if(x >= 500 && x<600)
+                                {
+                                    chosenPiece = 'B';
+                                }
+                                if(chosenPiece != ' ')
+                                {
+                                    if(!whitePromo)
+                                    {
+                                        chosenPiece = tolower(chosenPiece);
+                                    }
+                                    b[start.first][start.second] = chosenPiece;
+                                    promo = false;
+                                }
+                            }
+                            continue;
+                        }
+
+
+
                         if(turn && isWhite(b[start.first][start.second]))
                         {
                             moveFunc(b, start, end);
@@ -198,16 +266,33 @@ int main()
                         s = &bP[0];
                         break;
                 }
-
                 if(s != nullptr)
                 {
                     s->setPosition(j*100.0f, i*100.0f);
                     win.draw(*s);
                 }
-
             }
-
         }
-        win.display();
+        if(promo)
+        {
+            win.draw(promoMenu);
+
+            if(whitePromo)
+            {
+                win.draw(wpq);
+                win.draw(wpr);
+                win.draw(wpn);
+                win.draw(wpb);
+            }
+            else
+            {
+                win.draw(bpq);
+                win.draw(bpr);
+                win.draw(bpn);
+                win.draw(bpb);
+            }
+            win.display();
+        }
     }
 }
+
